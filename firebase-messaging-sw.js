a370b046c28f6adcfa660bc07d0e02173fb9874f
diesel8603 <dieselllllllll89@gmail.com>
2025-07-24 00:01:01 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,19 +1,25 @@
-importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js');
+// firebase-messaging-sw.js
+importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyBXXXCLGe70id54wlMhUtHQHOJe8l4a6wA",
   authDomain: "live-chat-9d81c.firebaseapp.com",
   projectId: "live-chat-9d81c",
   messagingSenderId: "253242248304",
-  appId: "1:253242248304:web:3e440995fe27bc8e2fb8b5",
+  appId: "1:253242248304:web:3e440995fe27bc8e2fb8b5"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(payload.notification.title, {
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icon.png' // varsa ikon
-  });
+    icon: '/icon.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
